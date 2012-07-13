8a35355051297d24601c6bbe41a092d056567c35
Adam Kochanowicz <ajkochanowicz@gmail.com>
2012-07-13 16:00:24 -0400

    modifying kickstrap() to check if function exists first.

--- Kickstrap/js/kickstrap.js
+++ Kickstrap/js/kickstrap.js
@@ -135,7 +135,10 @@
 		function setKickstrap() {
 		// Once all the plugins have loaded and had time to take off their shoes and have a martini, look at the user's custom stuff.
 			$(window).load(function() {
-				kickstrap();
+				if(typeof window.kickstrap == 'function') {
+					// function exists, so we can now call it
+					kickstrap();
+				}
 			});
 		}
 	}
