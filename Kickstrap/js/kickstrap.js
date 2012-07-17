3fd859e87996efe6062fce00fdf7b8af72ec8f4e
Adam Kochanowicz <ajkochanowicz@gmail.com>
2012-07-16 23:47:40 -0400

    about to do something drastic to support stupid IE

--- Kickstrap/js/kickstrap.js
+++ Kickstrap/js/kickstrap.js
@@ -1,5 +1,37 @@
 // Allow the user to change the root directory to automatically fix the directory of all their plugins.
 var rootDir, addOnLocation, customFunction;
+var selectorName = 'content';
+
+// Workaround for IE8 Content bug http://stackoverflow.com/questions/2519640/divmydiv-csscontent-returns-undefined-in-ie8
+// Test here: http://jsfiddle.net/ajkochanowicz/wC5NL/1/
+checkVersion();      
+function getInternetExplorerVersion() {
+  var rv = -1; // Return value assumes failure.
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+// If we're running IE8, we should look for the fake 'ie8' css property.
+function checkVersion() {
+  var ver = getInternetExplorerVersion();
+  if ( ver > -1 )
+  {
+    if ( ver <= 8.0 && ver > 7.0) 
+      selectorName = 'ie8'
+    else
+      selectorName = 'content';
+  }
+
+	/*$('script').each(function() {
+		this.setAttribute("style","ie8: 'blah'");
+	});*/
+
+}
 
 	(function($){  
 	 $.kickstrap = function(options) { 
@@ -90,7 +122,7 @@ var rootDir, addOnLocation, customFunction;
 			document.write('<script id="console" type="text/javascript">appendMagic(null, \'#console\');</script><script id="caching" type="text/javascript">appendMagic(null, \'#caching\');</script>');
 		
 			// Get the formatted content string
-			var contentString = stripslashes($('#addOns').css('content'));
+			var contentString = stripslashes($('#addOns').css(selectorName));
 			
 			var addOnArray = contentString.splitCSV();
 			// Once all the addOns are loaded, fire the user's handmade javascript from $.kickstrap.
@@ -116,7 +148,7 @@ var rootDir, addOnLocation, customFunction;
 				str=str.substring(1,str.length-1);
 				return str;
 			}
-			document.write(formatAppendee($(newAppendee).css('content')));
+			document.write(formatAppendee($(newAppendee).css(selectorName)));
 		}
 		
 		function loadScript(url, callback) {
--- product/Kickstrap/js/kickstrap.js
+++ product/Kickstrap/js/kickstrap.js
@@ -1,5 +1,37 @@
 // Allow the user to change the root directory to automatically fix the directory of all their plugins.
-var rootDir, addOnLocation;
+var rootDir, addOnLocation, customFunction;
+var selectorName = 'content';
+
+// Workaround for IE8 Content bug http://stackoverflow.com/questions/2519640/divmydiv-csscontent-returns-undefined-in-ie8
+// Test here: http://jsfiddle.net/ajkochanowicz/wC5NL/1/
+checkVersion();      
+function getInternetExplorerVersion() {
+  var rv = -1; // Return value assumes failure.
+  if (navigator.appName == 'Microsoft Internet Explorer')
+  {
+    var ua = navigator.userAgent;
+    var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+      rv = parseFloat( RegExp.$1 );
+  }
+  return rv;
+}
+// If we're running IE8, we should look for the fake 'ie8' css property.
+function checkVersion() {
+  var ver = getInternetExplorerVersion();
+  if ( ver > -1 )
+  {
+    if ( ver <= 8.0 && ver > 7.0) 
+      selectorName = 'ie8'
+    else
+      selectorName = 'content';
+  }
+
+	/*$('script').each(function() {
+		this.setAttribute("style","ie8: 'blah'");
+	});*/
+
+}
 
 	(function($){  
 	 $.kickstrap = function(options) { 
@@ -11,14 +43,8 @@ var rootDir, addOnLocation;
 	  };  
 	  var options = $.extend(defaults, options); 
 	  var rootDir = options.kickstrapIsHere;
-	  
+	  customFunction = options.customJS;
 	  appendMagic(rootDir);
-	  
-	  // Wait for all plugins to be loaded
-	 	$(window).load(function() {
-				var customFunction = options.customJS;
-				customFunction(); 
-	 	}); 
 	 };  
 	})(jQuery);
 	
@@ -93,13 +119,14 @@ var rootDir, addOnLocation;
 		if (!newAppendee) {
 		
 			// Write the appendMagic script elements for console, console tools, etc.
-			document.write('<script id="console" type="text/javascript">appendMagic(null, \'#console\');</script><script id="caching" type="text/javascript">appendMagic(null, \'#caching\');</script><script id="console-tools" type="text/javascript">appendMagic(null, \'#console-tools\');</script>');
+			document.write('<script id="console" type="text/javascript">appendMagic(null, \'#console\');</script><script id="caching" type="text/javascript">appendMagic(null, \'#caching\');</script>');
 		
 			// Get the formatted content string
-			var contentString = stripslashes($('#addOns').css('content'));
+			var contentString = stripslashes($('#addOns').css(selectorName));
 			
 			var addOnArray = contentString.splitCSV();
-			for (i=0;i<addOnArray.length || function(){ consoleLog('All addons linked'); return false;}();i++) {
+			// Once all the addOns are loaded, fire the user's handmade javascript from $.kickstrap.
+			for (i=0;i<addOnArray.length || function(){ consoleLog('Add-on linking complete.'); customFunction(); return false;}();i++) {
 				
 				// Open the add-on's config file to see what we need to import.
 				// But first make sure it's not commented out.
@@ -121,7 +148,7 @@ var rootDir, addOnLocation;
 				str=str.substring(1,str.length-1);
 				return str;
 			}
-			document.write(formatAppendee($(newAppendee).css('content')));
+			document.write(formatAppendee($(newAppendee).css(selectorName)));
 		}
 		
 		function loadScript(url, callback) {
